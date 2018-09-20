a1588778403956189e3bdb495de0dfeb39f19da1
Nathan <ntugwell@petsathome.co.uk>
2018-09-20 10:49:30 +0100

    Input accessory view and IBDesignable

--- window-shopper/Controller/MainViewController.swift
+++ window-shopper/Controller/MainViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    @IBOutlet weak var hourlyWageTextField: CustomTextField!
+    @IBOutlet weak var priceTextField: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 50))
+        calcButton.backgroundColor = #colorLiteral(red: 0.04350364953, green: 0.7325239182, blue: 0.1820122302, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
+        hourlyWageTextField.inputAccessoryView = calcButton
+        priceTextField.inputAccessoryView = calcButton
     }
 
+    @objc func calculate() {
+
+    }
 
 }
 
--- window-shopper/Views/CustomTextField.swift
+++ window-shopper/Views/CustomTextField.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
+@IBDesignable
 class CustomTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
     override func awakeFromNib() {
+        customizeView()
+    }
+    
+    func customizeView() {
         super.awakeFromNib()
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
@@ -21,8 +30,6 @@ class CustomTextField: UITextField {
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
     }
 
 }
