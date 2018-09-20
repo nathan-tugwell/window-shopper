8a9160ac079b70b6540e7aa1ed0fb35df150ec68
Nathan <ntugwell@petsathome.co.uk>
2018-09-20 14:56:46 +0100

    Draw using CGRect

--- window-shopper/Views/CustomTextField.swift
+++ window-shopper/Views/CustomTextField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CustomTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.1094389781, green: 0.1094650105, blue: 0.1094355509, alpha: 0.7883936216)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +40,7 @@ class CustomTextField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
