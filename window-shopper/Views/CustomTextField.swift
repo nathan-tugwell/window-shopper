37653b69ef38a7df17adaa1b7c126a25564e12e9
Nathan <ntugwell@petsathome.co.uk>
2018-09-20 08:34:27 +0100

    Lesson-One Custom textfield

---
+++ window-shopper/Views/CustomTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTextField.swift
+//  window-shopper
+//
+//  Created by Nathan Tugwell on 20/09/2018.
+//  Copyright © 2018 petsathome. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+
+}
